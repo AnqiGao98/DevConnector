1629a23f23eb10c5ad675ddc45eaf647b0e3514a
angel <gaoanqi0609@gmail.com>
2020-05-14 17:34:26 -0400

    Authentication done

--- config/db.js
+++ config/db.js
@@ -1,13 +1,16 @@
-const mongoose = require("mongoose");
-const config = require("config");
+const mongoose = require('mongoose');
+const config = require('config');
 
-const db = config.get("mongoURI");
+const db = config.get('mongoURI');
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(db, { useNewUrlParser: true });
-
-    console.log("MongoDB is connected.");
+    await mongoose.connect(db, {
+      useNewUrlParser: true,
+      useCreateIndex: true,
+      useUnifiedTopology: true,
+    });
+    console.log('MongoDB is connected.');
   } catch (error) {
     console.error(error.message);
 
